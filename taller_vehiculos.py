fac2fa3e12fe9870cc9da8c881a83147c5c5dedc
bizzottogregorio@gmail.com <bizzottogregorio@gmail.com>
2026-04-20 16:30:36 -0300

    subo autos

--- taller_vehiculos.py
+++ taller_vehiculos.py
@@ -15,6 +15,6 @@ class Auto(vehiculo):
 class camioneta(vehiculo):
     def __init__(self, marca, nombre, modelo,cilindrada, problema) -> None:
         super().__init__(marca, nombre, modelo,cilindrada, problema)
-        pass
+    pass
 
 auto_uno=Auto("chevrolet","corsa",2004,1.4,"correa de distribucion cortada y arbol de levas posiblemente doblado ")
