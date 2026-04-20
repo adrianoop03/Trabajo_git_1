58d24681101be013dee27b70b463cc81c917f965
bizzottogregorio@gmail.com <bizzottogregorio@gmail.com>
2026-04-20 16:01:22 -0300

    subo autos

--- taller_cliente.py
+++
@@ -1,6 +0,0 @@
-class cliente:
-    def __init__(self,marca,modelo,nombre,problema):
-        self.marca=marca
-        self.modelo=modelo
-        self.nombre=nombre
-        self.problema=problema
